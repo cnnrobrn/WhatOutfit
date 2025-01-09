8ebeb05ddb61a550affebefa8adaa9f39d841b6f
cnnrobrn <141182288+cnnrobrn@users.noreply.github.com>
2025-01-09 15:59:43 -0800

    test

---
+++ WhatOutfit/Background/GIFImage.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+import WebKit
+
+struct GIFImage: UIViewRepresentable {
+    let name: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.backgroundColor = .clear
+        webView.isOpaque = false
+        webView.scrollView.isScrollEnabled = false
+        webView.configuration.allowsInlineMediaPlayback = true
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        if let gifURL = Bundle.main.url(forResource: name, withExtension: "gif") {
+            let request = URLRequest(url: gifURL)
+            webView.load(request)
+        }
+    }
+}
